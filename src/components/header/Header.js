4864b52ec73132e0fcdb1d6fb322cb7f4ff3e464
stevenmckinnon <mckinnon92@gmail.com>
2020-03-12 19:00:20 +0000

    updated packages and fixed an a11y issue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
             <header id="home">
                 <nav id="nav-wrap">
                     <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-                    <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+                    <a className="mobile-btn" href="#hide-nav" title="Hide navigation">Hide navigation</a>
                     <ul id="nav" className="nav">
                         <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                         <li><a className="smoothscroll" href="#about">About</a></li>
